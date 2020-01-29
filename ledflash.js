38a05046ec5ae9f2821ddc8c6515a2cb2ef8f1bf
Tane10 <tane2@hotmail.co.uk>
2020-01-29 22:06:43 +0000

    trying to flash led

--- ledflash.js
+++ ledflash.js
@@ -1,1 +1,23 @@
-console.log("im working")
+const five = require("johnny-five");
+const raspi = require("raspi");
+const gpio = require("raspi-gpio");
+const led = require("raspi-led")
+
+var board = new five.Board();
+
+board.on("ready", () => {
+    raspi.init(() => {
+        const statusLed = new led.LED(7)
+      
+        // Flash the LED twice a second
+        setInterval(() => {
+          if (statusLed.read() == led.ON) {
+            statusLed.write(led.OFF); // Turn off the status LED
+          } else {
+            statusLed.write(led.ON); // Turn on the status LED
+          }
+        }, 500);
+      });
+});
+
+
